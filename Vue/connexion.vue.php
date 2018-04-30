b02133b60dd3525dc22a098bd5ab9a4fe48b44af
LONGUEMARE Florent <longuemf@hostname.iut2.upmf-grenoble.fr>
2018-04-30 16:51:57 +0200

    Création des modeles de classes et de la connexion

---
+++ Vue/connexion.vue.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" type="text/css" href="connexion.vue.css" media="screen"/>
+    <title>Page de connexion</title>
+  </head>
+  <body>
+    <fieldset>
+      <h1>Se connecter</h1>
+      <label for="champ_login">Login:</label>
+      <input type="text" name="login" id="champ_login" />
+      <br />
+      <label for="champ_mot_de_passe">Mot de passe:</label>
+      <input type="text" name="mot de passe" id="champ_mot_de_passe" />
+      <p>
+        <input type="submit" value="Annuler" >
+        <input type="submit" value="Valider" >
+      </p>
+    </fieldset>
+  </body>
+</html>
