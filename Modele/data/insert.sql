01cc256eace7746daa28bbcfda9ddd6baebf986d
SIMONETTA Guillaume <simonegu@hostname.iut2.upmf-grenoble.fr>
2018-05-03 13:31:07 +0200

    Mis en place du css

--- Modele/data/insert.sql
+++ Modele/data/insert.sql
@@ -1,29 +1,32 @@
 INSERT INTO membre(nom,prenom,pseudo,dateNaissance,adresseMail,numeroTel,motDePasse,avatar)
-VALUES ('ADMIN','admin','admin','06/12/1994','qsdq@qsdqsd','0668316514','admin','URL');
+VALUES ('ADMIN','admin','admin','06/12/1994','qsdq@qsdqsd','0668316514','admin','avatar-1.jpg');
 
 INSERT INTO membre(nom,prenom,pseudo,dateNaissance,adresseMail,numeroTel,motDePasse,avatar)
-VALUES ('REYMOND','Lucas','zFunHD','06/12/1994','qsdq@qsdqsd','0668316514','test','URL');
+VALUES ('REYMOND','Lucas','zfunHD','06/12/1994','vcb@qsdqsd','0668316514','admin','avatar-2.jpg');
 
 INSERT INTO membre(nom,prenom,pseudo,dateNaissance,adresseMail,numeroTel,motDePasse,avatar)
-VALUES ('CHEMIN','Ludivine','Yarps','1994-12-12','sdfs@sdfsdf','066831945','test','URL');
+VALUES ('GUEVARA','Gabrielle','laNulle','06/12/1994','qsdq@qsdzerqsd','0668316514','admin','avatar-1.jpg');
 
 INSERT INTO membre(nom,prenom,pseudo,dateNaissance,adresseMail,numeroTel,motDePasse,avatar)
-VALUES ('Tonton','Phil','master','1994-12-08','qsd@aaaaa','0668316514','test','URL');
+VALUES ('SIMONETTA','Guillame','emalliuG','06/12/1994','qsdq@qsdbn,sd','0668316514','admin','avatar-1.jpg');
+
+INSERT INTO membre(nom,prenom,pseudo,dateNaissance,adresseMail,numeroTel,motDePasse,avatar)
+VALUES ('LONGUEMIARE','Florent','tnerolF','06/12/1994','qsdq@qsdqazesd','0668316514','admin','avatar-1.jpg');
 
 
--- INSERT INTO voiture(numeroImmatriculation,marque,modele,couleur,proprietaire)
--- VALUES ('ZQ-124-PQ','Opel','Corsa','rouge',2);
---
--- INSERT INTO voiture(numeroImmatriculation,marque,modele,couleur,proprietaire)
--- VALUES ('XQ-124-PQ','Opel','Astra','rouge',3);
---
--- INSERT INTO MESSAGE(description,expediteur,destinataire)
--- VALUES ('C un message d insulte',1,2);
---
--- INSERT INTO MESSAGE(description,expediteur,destinataire)
--- VALUES ('C est une reponse',2,1);
---
---
+INSERT INTO voiture(numeroImmatriculation,marque,modele,couleur,proprietaire)
+VALUES ('ZQ-124-PQ','Opel','Corsa','rouge',2);
+
+INSERT INTO voiture(numeroImmatriculation,marque,modele,couleur,proprietaire)
+VALUES ('XQ-124-PQ','Opel','Astra','rouge',3);
+
+INSERT INTO MESSAGE(description,expediteur,destinataire)
+VALUES ('C un message d insulte',1,2);
+
+INSERT INTO MESSAGE(description,expediteur,destinataire)
+VALUES ('C est une reponse',2,1);
+
+
 INSERT INTO VILLE(codePostal,nom)
 VALUES ('38000','Grenoble');
 
@@ -35,20 +38,23 @@ VALUES ('38800','Pont de Claix');
 
 --
 INSERT INTO TRAJET(conducteur,description,prix,nombrePassagerMax,estimation,dateDepart,villeDepart,villeArrivee)
-VALUES (1,'un petit trajet',15,5,3,'2018-12-05',38000,38800);
+VALUES (2,'Voyage de grenoble à Lyon',50,3,120,'2018-12-05',38000,69000);
 
 INSERT INTO TRAJET(conducteur,description,prix,nombrePassagerMax,estimation,dateDepart,villeDepart,villeArrivee)
-VALUES (3,'un petit vers Grenoble',15,5,3,'2019-12-05',38000,38800);
+VALUES (3,'Voyage de Grenoble à Pont de Claix',10,2,20,'2018-12-05',38000,38800);
+
+INSERT INTO TRAJET(conducteur,description,prix,nombrePassagerMax,estimation,dateDepart,villeDepart,villeArrivee)
+VALUES (3,'Voyage de Four à Grenoble',45,5,95,'2019-12-05',38080,38000);
 
 
 INSERT INTO TRAJET(conducteur,description,prix,nombrePassagerMax,estimation,dateDepart,villeDepart,villeArrivee)
-VALUES (2,'un petit trajet',15,5,3,'2018-12-05',38000,38120);
-
-INSERT INTO DEMANDE(demandeur,trajet)
-VALUES (2,1);
-
-INSERT INTO DEMANDE(demandeur,trajet)
-VALUES (2,2);
+VALUES (5,'Voyage de Bonnefamille à Grenoble',15,5,3,'2018-12-05',3809,38000);
+--
+-- INSERT INTO DEMANDE(demandeur,trajet)
+-- VALUES (2,1);
+--
+-- INSERT INTO DEMANDE(demandeur,trajet)
+-- VALUES (2,2);
 
 --Valeurs de l'admin et quelques villes pour commencer
 -- INSERT INTO membre(nom,prenom,pseudo,dateNaissance,adresseMail,numeroTel,motDePasse,avatar)
