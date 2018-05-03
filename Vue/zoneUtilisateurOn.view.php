0a19ecaff2c9f06a228881bac01eb5c6101550ea
SIMONETTA Guillaume <simonegu@hostname.iut2.upmf-grenoble.fr>
2018-05-03 10:09:54 +0200

    Fin

--- Vue/zoneUtilisateurOn.view.php
+++ Vue/zoneUtilisateurOn.view.php
@@ -4,26 +4,11 @@
     <h1>Stud' & Go ! </h1>
 
   </div>
-
-
  <div id="messageBienvenue">
    <img src="<?php echo "../Ressource/avatar/$this->avatarMembre"?>" alt="Avatar du membre">
-<<<<<<< HEAD
    <p>Heureux de vous voir <?=$this->pseudoMembre ?></p>
-   <a href="deconnexion.controler.php">Déconnexion</a>
-   <?php
-   if ($this->idMembre==1) {
-     // code...
-     echo "<a href='../Controleur/adminBackOffice.controler.php'>Administration</a>";
-   } ?>
-
-=======
-   <p>Heureux de vous voir <br> <?=$this->pseudoMembre ?></p>
-   <a href="deconnexion.controler.php">
-     <div class="boutonsLogin">
-       <br>Déconnexion
-     </div></a>
->>>>>>> d041412a181d384f6a7077e52479259bf3a209e4
- </div>
+   <a href="deconnexion.controler.php"><div class="boutonsLogin">
+     <br>Déconnexion
+   </div>
 
 </div>
